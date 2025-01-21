aad55b30d83d1f59e5c77e67ab7db22445d7d4a5
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2025-01-21 14:11:20 +0100

    First compil check

--- examples/images/exampleDebug.cpp
+++ examples/images/exampleDebug.cpp
@@ -11,8 +11,76 @@
 #include "DGtal/images/ImageContainerBySTLVector.h"
 
 #include "DGtal/io/readers/GenericReader.h"
-#include "DGtal/io/writers/GenericWriter.h"
+#include "DGtal/base/BasicFunctors.h"
 
+namespace DGtal { namespace concepts {
+namespace Override
+{
+    template <typename I>
+    struct CTrivialConstImage: concepts::CPointFunctor<I>
+    {
+
+    public:
+
+      BOOST_CONCEPT_ASSERT((CLabel<typename I::Value>));
+      //Inner types
+      typedef typename I::Domain Domain;
+      BOOST_CONCEPT_ASSERT((concepts::CDomain<Domain>));
+
+
+      BOOST_CONCEPT_USAGE(CTrivialConstImage)
+      {
+        ConceptUtils::sameType(i.domain(), d);
+      }
+
+    private:
+      I i;
+      Domain d;
+
+    };
+
+    template <typename I>
+    struct CConstImage // : CTrivialConstImage<I>
+    {
+
+    public:
+      typedef typename I::Domain Domain;
+      BOOST_CONCEPT_ASSERT((concepts::CDomain<Domain>));
+
+      typedef typename I::ConstRange ConstRange;
+      BOOST_CONCEPT_ASSERT((CConstBidirectionalRangeFromPoint<ConstRange>));
+
+      BOOST_CONCEPT_USAGE(CConstImage)
+      {
+        ConceptUtils::sameType(i.domain(), d);
+        ConceptUtils::sameType(i.constRange(), r);
+      }
+
+    private:
+      I i;
+      Domain d;
+      ConstRange r;
+    };
+} } }
+
+template <typename TContainer,
+          int Tdim =TContainer::Point::dimension,
+          typename TValue = typename TContainer::Value,
+          typename TFunctor = DGtal::functors::Identity >
+struct GenericWriter
+{
+  BOOST_CONCEPT_ASSERT((  DGtal::concepts::Override::CConstImage<TContainer> )) ;
+  /**
+   * Export an  image.
+   * @param filename the filename of the saved image (with a extension name).
+   * @param anImage the image to be saved.
+   * @param aFunctor to apply image transformation before saving.
+   *
+   **/
+  static bool exportFile(const std::string &filename,
+                         const TContainer &anImage,
+                         const TFunctor & aFunctor = TFunctor() ) { return false; }
+};
 ///////////////////////////////////////////////////////////////////////////////
 
 using namespace std;
@@ -48,7 +116,7 @@ int main( int /*argc*/, char** /*argv*/ )
     subSampler3D.getSubSampledDomain(), 
     subSampler3D, 
     df);
-  // GenericWriter<ConstImageAdapterForSubSampling3D>::exportFile("", subsampledImage3D );
+  GenericWriter<ConstImageAdapterForSubSampling3D>::exportFile("", subsampledImage3D );
   return 0;
 }
 //                                                                           //
