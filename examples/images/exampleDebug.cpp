1223ad15bcad57c12093e6ce7428b83f6671b704
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2025-01-21 15:25:18 +0100

    Unroll requirements

--- examples/images/exampleDebug.cpp
+++ examples/images/exampleDebug.cpp
@@ -17,40 +17,23 @@ namespace DGtal { namespace concepts {
 namespace Override
 {
     template <typename T>
-    struct CConstBidirectionalRange // : CConstSinglePassRange<T>
-    {
-      // ----------------------- Concept checks ------------------------------
-    public:
-      typedef typename T::ConstReverseIterator ConstReverseIterator;
-      BOOST_CONCEPT_ASSERT(( boost_concepts::SinglePassIteratorConcept<ConstReverseIterator> ));
-  
-      BOOST_CONCEPT_USAGE(CConstBidirectionalRange)
-      {
-        concepts::ConceptUtils::sameType( it, i.rbegin() );
-        concepts::ConceptUtils::sameType( it, i.rend() );
-      }
-    private:
-      T i;
-      ConstReverseIterator it;
-    }; // end of concept CConstBidirectionalRange
-
-    template <typename T>
-    struct CConstBidirectionalRangeFromPoint2 // : CConstBidirectionalRange<T>
+    struct CConstBidirectionalRangeFromPoint
     {
       // ----------------------- Concept checks ------------------------------
     public:
       typedef typename T::Point Point;
-      BOOST_CONCEPT_USAGE( CConstBidirectionalRangeFromPoint2 )
+      BOOST_CONCEPT_USAGE( CConstBidirectionalRangeFromPoint )
       {
         concepts::ConceptUtils::sameType( myB, myX.rbegin( myPoint ) );
       }
-      
+
       // ------------------------- Private Datas --------------------------------
     private:
       T myX;
       Point myPoint;
       typename T::ConstReverseIterator myB;
 
+      // I::ConstRange::ConstReverseIterator === I::ConstRange::rebgin()
       // ------------------------- Internals ------------------------------------
     private:
 
@@ -61,7 +44,7 @@ namespace Override
     {
     public:
       typedef typename I::ConstRange ConstRange;
-      BOOST_CONCEPT_ASSERT((CConstBidirectionalRangeFromPoint2<ConstRange>)); 
+      BOOST_CONCEPT_ASSERT((CConstBidirectionalRangeFromPoint<ConstRange>)); 
     private:
     };
 
@@ -90,6 +73,9 @@ struct GenericWriter
 using namespace std;
 using namespace DGtal;
 
+template<typename T>
+void f(T&& parameter);  // purposefully not defined
+
 ///////////////////////////////////////////////////////////////////////////////
 
 int main( int /*argc*/, char** /*argv*/ )
@@ -100,6 +86,8 @@ int main( int /*argc*/, char** /*argv*/ )
                             functors::BasicDomainSubSampler<Image3D::Domain>,
                             Image3D::Value, 
                             functors::Identity > ConstImageAdapterForSubSampling3D;
+
+
   //! [imageBasicSubsamplingType3D]
   functors::Identity df;
   Image3D image3D = GenericReader<Image3D>::import( "" );
@@ -110,7 +98,16 @@ int main( int /*argc*/, char** /*argv*/ )
     subSampler3D.getSubSampledDomain(), 
     subSampler3D, 
     df);
-  GenericWriter<ConstImageAdapterForSubSampling3D>::exportFile("", subsampledImage3D );
+  // GenericWriter<ConstImageAdapterForSubSampling3D>::exportFile("", subsampledImage3D );
+
+  using ConstRange = typename ConstImageAdapterForSubSampling3D::ConstRange;
+  using Expected   = typename ConstRange::ConstReverseIterator;
+  
+  auto r  = subsampledImage3D.constRange();
+  auto rr = r.rbegin(Z3i::Point{});
+  
+  static_assert(std::is_same_v<Expected, decltype(rr)>);
+  
   return 0;
 }
 //                                                                           //
