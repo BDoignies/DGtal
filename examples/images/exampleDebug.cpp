31ae9da3efb24f14d1eb7aff66bccb2722c2f19c
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2025-01-23 11:28:36 +0100

    Test

--- examples/images/exampleDebug.cpp
+++ examples/images/exampleDebug.cpp
@@ -11,10 +11,27 @@ void f(T&& parameter);  // purposefully not defined
 
 ///////////////////////////////////////////////////////////////////////////////
 
+#include "DGtal/kernel/BasicPointFunctors.h"
+#include "DGtal/images/ConstImageAdapter.h"
+#include "DGtal/images/ImageContainerBySTLVector.h"
+
+template<typename T>
+void f(T&& v);
+
 int main( int /*argc*/, char** /*argv*/ )
 {
-  Z3i::Domain::ConstIterator it({}, {}, {});
-  std::reverse_iterator<Z3i::Domain::ConstIterator> ri(it);
+  typedef ImageContainerBySTLVector < Z2i::Domain, unsigned char> Image2D;
+  typedef ConstImageAdapter<Image2D,  Image2D::Domain, 
+                            functors::BasicDomainSubSampler<Image2D::Domain>,  
+                            Image2D::Value,
+                            functors::Identity > ConstImageAdapterForSubSampling;
+
+  Image2D im(Z2i::Domain{});
+  ConstImageAdapterForSubSampling* aa;
+  ConstImageAdapterForSubSampling::ConstRange r(aa);
+  f(r);
+  // Z3i::Domain::ConstIterator it({}, {}, {});
+  // std::reverse_iterator<Z3i::Domain::ConstIterator> ri(it);
   
   return 0;
 }
