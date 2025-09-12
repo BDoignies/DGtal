9f7b508e47ef594cb3711576ea8f4568f17288d7
Bastien DOIGNIES <bastien.doignies@liris.cnrs.fr>
2025-09-12 14:23:30 +0200

    FixAttempt

--- cmake/deps/boost.cmake
+++ cmake/deps/boost.cmake
@@ -9,7 +9,7 @@
 # OF ANY KIND, either express or implied. See the License for the specific language
 # governing permissions and limitations under the License.
 #
-if(TARGET Boost::headers)
+if(TARGET Boost::headers OR TARGET boost_headers OR TARGET Boost::boost)
     return()
 endif()
 
